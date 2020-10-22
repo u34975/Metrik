d0a9a785fb08595c6d8ca82450eb7bcb490cf364
Jana Apfelhofer <u34975@hs-harz.de>
2020-10-22 17:46:52 +0200

    Version1.0.1

---
+++ SoftwaretechnikVersion1.0.1/MainMenuBar.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.awt.*;
+
+public class MainMenuBar extends MenuBar {
+
+    private Menu _menu;
+
+    public MainMenuBar(){
+        _menu = new Menu("Hintergrundfarbe-Programm");
+        _menu.add(new MenuItem("Farbe: rot"));
+        _menu.add(new MenuItem("Farbe: gelb"));
+        _menu.add(new MenuItem("Farbe: grün"));
+
+        add(_menu);
+    }
+
+}
---
+++ SoftwaretechnikVersion1.0.1/MainWindow.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainWindow extends Frame implements WindowListener{
+
+    public MainWindow(){
+        setTitle("Programm für Softwaretechnik");
+        setSize(500, 300);
+
+        addWindowListener(this);
+        add(new DrawObject());
+        setMenuBar(new MainMenuBar());
+    }
+
+    public void openWindow(){
+        setVisible(true);
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        dispose();
+        System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+}
